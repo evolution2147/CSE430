ae53c0cf50fcc6bacb45182fcc0c44381071cf5a
Adam Frankowski <adam@Adams-MacBook-Pro-2.local>
2015-02-08 13:45:14 -0700

    hello world

---
+++ proj_1.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  430_1
+//
+//  Created by Adam Frankowski on 2/8/15.
+//  Copyright (c) 2015 Adam Frankowski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
